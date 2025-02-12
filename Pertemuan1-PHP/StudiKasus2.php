c2320daa5088a793c0614558f52f91085fb61d28
ThoriqAR1301 <thoriqazhar131@gmail.com>
2025-02-12 09:10:18 +0700

    Menambahkan File

---
+++ Pertemuan1-PHP/StudiKasus2.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Studi Kasus 2";
+echo "<hr>";
+
+$member = ""; // Jenis-Jenis Member : "Member-Gold", "Member-Silver", "Non-Member"
+$diskon = ""; // Menyesuaikan Kriteria Studi Kasus 2
+$belanja = ""; // Terserah
+
